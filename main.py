bd6ccfeda592fe97bf2eaeb7e0626cd2808bb608
Sergio Ferreira <sergio.ferreira@jscrambler.com>
2019-06-22 22:46:31 +0100

    now generates latex tables

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ from pathlib import Path
 for filename in Path('./').glob('**/*test.js'):
   file = open(filename, "r")
   testFile = file.name.split(".",1)[0]
-  print(testFile)
+
+  print('\\begin{table}[H]')
+  print('\\centering')
+  print('\\begin{tabular}{|l|l|l|}')
+  print('\\hline')
+  print('\\multicolumn{1}{|c|}{Caso de uso} & Descrição & Resultado \\\\ \hline')
 
   for line in file:
       if re.search('test', line):
           nomeTeste = re.findall(r"'(.*?)'", line, re.DOTALL)[0]
-
           if (nomeTeste != 'supertest'):
-            print(nomeTeste)
+            print('Teste & {0} & \\\\ \\hline'.format(nomeTeste))
+          
+  print('\\end{tabular}')
+  print('\\end{table}')
 
   file.close()
